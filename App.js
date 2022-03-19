c2bc7c958a69fad84de90e89edf81b67f755bca0
Sasha Kramer <sashasemail@gmail.com>
2022-03-19 10:17:01 -0400

    getting speed

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import Speed from './components/Speed'
+import * as Location from 'expo-location';
 
 export default function App() {
   // rgb(153,229,178) Pantone 352
   // rgb(181,226,191) Pantone 344
   // 
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <TailwindProvider utilities={utilities}>
-        <Speed />
+        <Speed location={location} />
     </TailwindProvider>
   );
 }
--- components/Speed/index.js
+++ components/Speed/index.js
@@ -2,15 +2,25 @@ import { useState, useEffect } from 'react'
 import {useTailwind} from 'tailwind-rn';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function Speed(){
+export default function Speed(props){
+    const { location } = props
     const tailwind = useTailwind();
 
     const [speed, setSpeed] = useState(0)
+
+    useEffect(() => {
+        if(location?.coords && location.coords.speed){
+            setSpeed(Math.round(location.coords.speed))
+        }
+    }, [location])
 
     return (
         <SafeAreaView style={tailwind('bg-teal-400 h-full flex items-center justify-center')}>
         {/* <SafeAreaView style={styles.container}> */}
             <Text style={tailwind('text-xl text-neutral-100')}>Speed {speed}</Text>
+            {/* <Text>
+                {JSON.stringify(location)}
+            </Text> */}
         </SafeAreaView>
     )
 };
